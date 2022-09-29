18c447112cf2289f7508d5e4800eb224674e2ce2
Abitegeka-Jinno-Jackson <jinnoblessed@gmail.com>
2022-09-29 13:36:19 +0300

    Update more.dart

--- lib/more.dart
+++ lib/more.dart
@@ -13,7 +13,7 @@ class _MoreState extends State<More> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.greenAccent
+      backgroundColor: Colors.greenAccent,
       appBar: AppBar(
         title: const Text(" MORE"),
       ),
