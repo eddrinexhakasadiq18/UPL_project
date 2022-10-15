cb9957f1a322dace0a3c0856b48e894b812e2476
Abitegeka-Jinno-Jackson <jinnoblessed@gmail.com>
2022-10-15 17:45:30 +0300

    Stat
    My SECOND COMMIT

--- lib/stat.dart
+++ lib/stat.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import "package:flutter/material.dart";
 
 class Stat extends StatefulWidget {
@@ -18,8 +16,147 @@ class _StatState extends State<Stat> {
         title: const Text(" STATISTICS"),
       ),
       body: ListView(
-        children: const [
-          Text("THIS IS THE STAT PAGE"),
+        children: [
+          // Single Statistic of a player
+          Container(
+            height: 100,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: Color.fromARGB(188, 112, 212, 30),
+            ),
+            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    // Player Image
+                    const CircleAvatar(
+                      backgroundColor: Colors.red,
+                      foregroundImage: AssetImage("assets/images/kcca.png"),
+                    ),
+                    SizedBox(width: 20),
+
+                    // Player Name
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [Text("RONALD"), Text("Portugual")],
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    // Play Icon
+                    const Icon(
+                      Icons.play_arrow,
+                      size: 50,
+                    ),
+                    SizedBox(width: 12),
+
+                    // Hear Icon
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [Icon(Icons.heart_broken), Text("90")],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 100,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: Color.fromARGB(188, 112, 212, 30),
+            ),
+            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    // Player Image
+                    const CircleAvatar(
+                      backgroundColor: Colors.red,
+                      foregroundImage: AssetImage("assets/images/kcca.png"),
+                    ),
+                    SizedBox(width: 20),
+
+                    // Player Name
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [Text("RONALD"), Text("Portugual")],
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    // Play Icon
+                    const Icon(
+                      Icons.play_arrow,
+                      size: 50,
+                    ),
+                    SizedBox(width: 12),
+
+                    // Hear Icon
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [Icon(Icons.heart_broken), Text("90")],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        
+          Container(
+            height: 100,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: Color.fromARGB(188, 112, 212, 30),
+            ),
+            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    // Player Image
+                    const CircleAvatar(
+                      backgroundColor: Colors.red,
+                      foregroundImage: AssetImage("assets/images/kcca.png"),
+                    ),
+                    SizedBox(width: 20),
+
+                    // Player Name
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [Text("RONALD"), Text("Portugual")],
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    // Play Icon
+                    const Icon(
+                      Icons.play_arrow,
+                      size: 50,
+                    ),
+                    SizedBox(width: 12),
+
+                    // Hear Icon
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [Icon(Icons.heart_broken), Text("90")],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
